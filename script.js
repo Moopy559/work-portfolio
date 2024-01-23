1fc93b3df3a736b0eb4bce721e4fa6f439630b2c
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-01-23 15:47:43 +1000

    Add modal carousel logic and keydown/click logic for image carousel

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const modalObj = {
   modal: document.querySelector("div.modal"),
   modalExit: document.querySelector("#modalExit"),
   modalImg: document.querySelector("img.modal-content"),
+  leftArrow: document.querySelector("#leftArrow"),
+  rightArrow: document.querySelector("#rightArrow"),
   gridCont: document.querySelector("div.grid-container"),
   gridDivs: document.querySelectorAll(".grid-container div"),
-  imgCount: 0,
+  imgCount: 1,
   openModal: function () {
     this.gridDivs.forEach((div) => {
       div.addEventListener("click", () => {
@@ -13,6 +15,9 @@ const modalObj = {
         this.modalImg.setAttribute("src", div.firstChild.getAttribute("src"));
       });
     });
+    this.keyChangeImg();
+    this.clickLeftArrow();
+    this.clickRightArrow();
   },
   keepModal: function () {
     this.modalImg.addEventListener("click", () => {
@@ -28,23 +33,10 @@ const modalObj = {
     document.addEventListener("keydown", (e) => {
       switch (e.key) {
         case "ArrowLeft":
-          if (this.imgCount === 1) {
-            let lastImg = this.gridCont.childElementCount - 1;
-            this.imgCount =
-              this.gridCont.children[lastImg].firstChild.getAttribute("order") *
-              1;
-          } else if (this.imgCount > 1) {
-            this.imgCount--;
-          }
-          this.assignImg();
+          this.leftLogic();
           break;
         case "ArrowRight":
-          if (this.imgCount === this.gridCont.childElementCount) {
-            this.imgCount = 1;
-          } else {
-            this.imgCount++;
-          }
-          this.assignImg();
+          this.rightLogic();
           break;
       }
     });
@@ -53,9 +45,36 @@ const modalObj = {
     let currentImg = this.gridCont.querySelector(`[order="${this.imgCount}"]`);
     this.modalImg.setAttribute("src", currentImg.getAttribute("src"));
   },
+  leftLogic: function () {
+    if (this.imgCount === 1) {
+      let lastImg = this.gridCont.childElementCount - 1;
+      this.imgCount =
+        this.gridCont.children[lastImg].firstChild.getAttribute("order") * 1;
+    } else if (this.imgCount > 1) {
+      this.imgCount--;
+    }
+    this.assignImg();
+  },
+  rightLogic: function () {
+    if (this.imgCount === this.gridCont.childElementCount) {
+      this.imgCount = 1;
+    } else {
+      this.imgCount++;
+    }
+    this.assignImg();
+  },
+  clickLeftArrow: function () {
+    this.leftArrow.addEventListener("click", () => {
+      this.leftLogic();
+    });
+  },
+  clickRightArrow: function () {
+    this.rightArrow.addEventListener("click", () => {
+      this.rightLogic();
+    });
+  },
 };
 
 modalObj.openModal();
 modalObj.keepModal();
 modalObj.exitModal();
-modalObj.keyChangeImg();
