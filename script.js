7b0437ebaa617f854cbdc82552deea4fc08fbbc6
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-01-23 11:27:31 +1000

    init commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const modalObj = {
+  modal: document.querySelector("div.modal"),
+  modalExit: document.querySelector("#modalExit"),
+  modalImg: document.querySelector("img.modal-content"),
+  gridCont: document.querySelector("div.grid-container"),
+  gridDivs: document.querySelectorAll(".grid-container div"),
+  imgCount: 0,
+  openModal: function () {
+    this.gridDivs.forEach((div) => {
+      div.addEventListener("click", () => {
+        this.modal.classList.remove("hidden");
+        this.imgCount = div.firstChild.getAttribute("order") * 1;
+        this.modalImg.setAttribute("src", div.firstChild.getAttribute("src"));
+      });
+    });
+  },
+  keepModal: function () {
+    this.modalImg.addEventListener("click", () => {
+      return;
+    });
+  },
+  exitModal: function () {
+    this.modalExit.addEventListener("click", () => {
+      this.modal.classList.toggle("hidden");
+    });
+  },
+  keyChangeImg: function () {
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          if (this.imgCount === 1) {
+            let lastImg = this.gridCont.childElementCount - 1;
+            this.imgCount =
+              this.gridCont.children[lastImg].firstChild.getAttribute("order") *
+              1;
+          } else if (this.imgCount > 1) {
+            this.imgCount--;
+          }
+          this.assignImg();
+          break;
+        case "ArrowRight":
+          if (this.imgCount === this.gridCont.childElementCount) {
+            this.imgCount = 1;
+          } else {
+            this.imgCount++;
+          }
+          this.assignImg();
+          break;
+      }
+    });
+  },
+  assignImg: function () {
+    let currentImg = this.gridCont.querySelector(`[order="${this.imgCount}"]`);
+    this.modalImg.setAttribute("src", currentImg.getAttribute("src"));
+  },
+};
+
+modalObj.openModal();
+modalObj.keepModal();
+modalObj.exitModal();
+modalObj.keyChangeImg();
